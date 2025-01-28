18268f3156d6cd76efc6113f526233a4799336fa
ARTBYDREAMZ <artbydreamz@gmail.com>
2025-01-27 18:31:10 -0800

    initalcommit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import { ToastProvider } from "@/components/ui/toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "NFT Minting template",
-	description: "A minting template powered by thirdweb",
+	title: "Abstract Exclusives",
+	description: "OPEN EDITION MINT",
 };
 
 export default function RootLayout({
--- src/components/nft-mint.tsx
+++ src/components/nft-mint.tsx
@@ -70,7 +70,7 @@ export function NftMint(props: Props) {
 			<div className="absolute top-4 right-4">
 				<ConnectButton client={client} />
 			</div>
-			<Card className="w-full max-w-md">
+			<Card className="max-w-full max-w-md">
 				<CardContent className="pt-6">
 					<div className="aspect-square overflow-hidden rounded-lg mb-4 relative">
 						{props.isERC1155 ? (
